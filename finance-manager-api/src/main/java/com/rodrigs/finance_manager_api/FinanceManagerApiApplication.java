bbb555c28e32097c4cee6a137178b4b694e31334
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-08-03 22:41:20 -0300

    feat: initialize project structure with Spring Boot setup and configuration files

---
+++ finance-manager-api/src/main/java/com/rodrigs/finance_manager_api/FinanceManagerApiApplication.java
@@ -0,0 +1,15 @@
+package com.rodrigs.finance_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceManagerApiApplication.class, args);
+
+		System.out.println("Hello World!");
+	}
+
+}
