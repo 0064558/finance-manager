034b611989be09c9adf345b1639b06e422694736
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-08-05 16:54:41 -0300

    feat: add Bearer authentication scheme to OpenAPI configuration

--- finance-manager-api/src/main/java/com/rodrigs/finance_manager_api/config/OpenApiConfig.java
+++ finance-manager-api/src/main/java/com/rodrigs/finance_manager_api/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.rodrigs.finance_manager_api.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
 
 	private static final String API_V1_PATH = "/api/v1";
+	private static final String BEARER_AUTH = "bearerAuth";
 
 	@Bean
 	public OpenAPI financeManagerOpenAPI() {
@@ -17,7 +21,14 @@ public class OpenApiConfig {
 				.info(new Info()
 						.title("Finance Manager API")
 						.description("API for managing personal finances.")
-						.version("v1"));
+						.version("v1"))
+				.components(new Components()
+						.addSecuritySchemes(BEARER_AUTH, new SecurityScheme()
+								.name(BEARER_AUTH)
+								.type(SecurityScheme.Type.HTTP)
+								.scheme("bearer")
+								.bearerFormat("JWT")))
+				.addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH));
 	}
 
 	@Bean
