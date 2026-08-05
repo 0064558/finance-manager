d891210b360cdb44dc1ad7f50409a61025c55110
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-08-04 22:22:25 -0300

    feat: remove unused ResponseEntity import from AuthController

--- finance-manager-api/src/main/java/com/rodrigs/finance_manager_api/user/controller/AuthController.java
+++ finance-manager-api/src/main/java/com/rodrigs/finance_manager_api/user/controller/AuthController.java
@@ -5,7 +5,6 @@ import com.rodrigs.finance_manager_api.user.dto.UserResponseDTO;
 import com.rodrigs.finance_manager_api.user.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
